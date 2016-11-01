8fbae7303502d7b3ce2a3e56ca77561b28714ffa
vaucheret <vaucheret@gmail.com>
2016-11-01 14:57:29 -0300

    * CALP/rendevous2.png: fin de concurrencia

--- CALP/ciaoexep.pl
+++ CALP/ciaoexep.pl
@@ -1,9 +1,9 @@
 
 
- p(_X) :- throw(error), display('---').
- p(X) :- display(X).
+p(_X) :- throw(error), display('---').
+p(X) :- display(X).
 
-testcatch:- catch(p(0), E, display(E)), display(.), fail.
+testcatch:- catch(catch(p(0), E, display(E)), display(.),F,handle(F)), fail.
 
 q(_X) :- send_signal(error), display('---').
 q(X) :- display(X).
