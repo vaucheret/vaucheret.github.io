64c9995d04aab398acdf6b20383bb701947730e8
Claudio <cv@debian-8-amd64.fi.uncoma.edu.ar>
2020-10-27 13:10:13 -0300

    added slideios

---
+++ slidesio/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Concurrencia', 
+     subtitle: 'Conceptos Avanzados en Lenguajes de Programación', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/org-icon.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Claudio Vaucheret', 
+     company: 'Facultad de Informática', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: '', 
+   }]
+};
