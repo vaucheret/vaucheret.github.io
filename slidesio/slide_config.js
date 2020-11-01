15888ea3ca400ebd425f4e6a1ac51f7c2fed8679
Claudio <cv@debian-8-amd64.fi.uncoma.edu.ar>
2020-10-31 23:02:11 -0300

    slides conc y exep

--- slidesio/slide_config.js
+++ slidesio/slide_config.js
@@ -7,7 +7,7 @@ var SLIDE_CONFIG = {
      usePrettify: true, // Default: true 
      enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
      enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-     favIcon: 'images/org-icon.png', 
+     favIcon: 'images/fai.png', 
      fonts: [
        'Open Sans:regular,semibold,italic,italicsemibold',
        'Source Code Pro'
