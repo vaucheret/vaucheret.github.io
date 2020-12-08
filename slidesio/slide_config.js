d16bc54a5ec7e2583c55753ead27bc71f678576d
Claudio <cv@debian-8-amd64.fi.uncoma.edu.ar>
2020-12-08 17:07:11 -0300

    new reveal

--- slidesio/slide_config.js
+++ slidesio/slide_config.js
@@ -1,13 +1,13 @@
 var SLIDE_CONFIG = {
    // Slide Settings
    settings: {
-     title: 'Conceptos Avanzados en Lenguajes de Programación', 
+     title: 'Example Presentation', 
      subtitle: '', 
      useBuilds: true, // Default: true. False will turn off slide animation builds. 
      usePrettify: true, // Default: true 
      enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
      enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-     favIcon: 'images/fai.png', 
+     favIcon: 'images/emacs-icon.png', 
      fonts: [
        'Open Sans:regular,semibold,italic,italicsemibold',
        'Source Code Pro'
@@ -16,8 +16,8 @@ var SLIDE_CONFIG = {
  
    // Author information
    presenters: [{
-     name: 'Claudio Vaucheret', 
-     company: 'Facultad de Informática', 
+     name: 'Eric Schulte', 
+     company: '', 
      gplus: '', 
      twitter: '', 
      www: '', 
