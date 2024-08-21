8c741233a55139a1997eac56137d6dfd1bbb8f17
vaucheret <vaucheret@gmail.com>
2024-08-21 10:45:17 -0300

    tipos

--- CALP/semantic.pl
+++ CALP/semantic.pl
@@ -151,13 +151,13 @@ semanticaestatica -->
 			  "Sintaxis",
 			  item(
 			      [
-				  "\\( < assign > \\to < var > = < expr > \\)",
-				  "\\( < expr > \\to < var > + < var > | < var > \\)",
-				  "\\( < var > \\to A | B | C \\)"
+				  "\\( \\mbox{&lt;assign&gt;} \\to \\mbox{&lt;var&gt;} = \\mbox{&lt;expr&gt;}\\)",
+				  "\\( \\mbox{&lt;expr&gt;} \\to \\mbox{&lt;var&gt;} + \\mbox{&lt;var&gt;} | \\mbox{&lt;var&gt;} \\)",
+				  "\\( \\mbox{&lt;var&gt;} \\to A | B | C \\)"
 			      ])
 		      ),
-		      "tipo-real: sintetizado por < var > y < expr >",
-		      "tipo-esperado: heredado por < expr >"
+		      "tipo-real: sintetizado por \\(\\mbox{&lt;var&gt;}\\) y \\(\\mbox{&lt;expr&gt;}\\)",
+		      "tipo-esperado: heredado por \\(\\mbox{&lt;expr&gt;}\\)"
 		  ]),
 	      fig(img("attribgram1.png","auto","auto"))
 	  )),
@@ -165,38 +165,38 @@ semanticaestatica -->
 	      enum(
 		  [
 		      (
-			  "Regla sintáctica: \\( < assign > \\to < var > = < expr > \\)",
+			  "Regla sintáctica: \\( \\mbox{&lt;assign&gt;} \\to \\mbox{&lt;var&gt;} = \\mbox{&lt;expr&gt;} \\)",
 			  item(
 			      [
-				 "Regla semántica: \\( < expr >.tipo-esperado \\leftarrow < var >.tipo-real \\)"
+				 "Regla semántica: \\( \\mbox{&lt;expr&gt;.tipo-esperado} \\leftarrow \\mbox{&lt;var&gt;.tipo-real} \\)"
 			      ])
 		      ),
 		      (
-			  "Regla sintáctica: \\( < expr > \\to < var > [2] + < var > [3] \\)",
+			  "Regla sintáctica: \\( \\mbox{&lt;expr&gt;} \\to \\mbox{&lt;var&gt;[2]} + \\mbox{&lt;var&gt;[3]} \\)",
 			  item(
 			      [
 				  (
-				      "Regla semántica: \\( < expr >.tipo-real \\leftarrow \\)", 
+				      "Regla semántica: \\( \\mbox{&lt;expr&gt;.tipo-real} \\leftarrow \\)", 
 				      el(p, 
-					 "if \\( < var >[2].tipo-real = int \\)  and \\( < var >[3].tipo-real = int \\)  then \\( int \\) else \\( real \\)  end if  ")
+					 "if \\( \\mbox{&lt;var&gt;[2].tipo-real} = int \\)  and \\( \\mbox{&lt;var&gt;[3].tipo-real} = int \\)  then \\( int \\) else \\( real \\)  end if  ")
 				  ),
-				  "Predicado: \\( < expr >.tipo-real = < expr >.tipo-esperado \\)"
+				  "Predicado: \\( \\mbox{&lt;expr&gt;.tipo-real} = \\mbox{&lt;expr&gt;.tipo-esperado } \\)"
 			      ])
 			  
 		      ),
 		      (
-			  "Regla sintáctica: \\( < expr > \\to < var > \\)",
+			  "Regla sintáctica: \\( \\mbox{&lt;expr&gt;} \\to \\mbox{&lt;var&gt;} \\)",
 			  item(
 			      [
-				      "Regla semántica: \\( < expr >.tipo-real \\leftarrow < var >.tipo-real \\)", 
-				      "Predicado: \\( < expr >.tipo-real = < expr >.tipo-esperado \\)"
+				      "Regla semántica: \\( \\mbox{&lt;expr&gt;.tipo-real } \\leftarrow \\mbox{&lt;var&gt;.tipo-real } \\)", 
+				      "Predicado: \\( \\mbox{&lt;expr&gt;.tipo-real } = \\mbox{&lt;expr&gt;.tipo-esperado } \\)"
 			      ])
 		      ),
 		      (
-			  "Regla sintáctica: \\( < var > \\to A | B | C \\)",
+			  "Regla sintáctica: \\( \\mbox{&lt;var&gt;} \\to A | B | C \\)",
 			  item(
 			      [
-				      "Regla semántica: \\( < var >.tipo-real \\leftarrow \\) lookup \\( ( < var >.string) \\)"
+				      "Regla semántica: \\( \\mbox{&lt;var&gt;.tipo-real } \\leftarrow \\) lookup \\( ( \\mbox{&lt;var&gt;.string}) \\)"
 
 			      ])
 		      )
@@ -217,15 +217,15 @@ semanticaestatica -->
 	  (   
 	      enum(
 		  [
-		       "\\( < var >.tipo-real \\leftarrow look-up(A) \\)  (Regla 4)",
-		       "\\( < expr >.tipo-esperado \\leftarrow < var >.tipo-real \\) (Regla 1)",
+		       "\\( \\mbox{&lt;var&gt;.tipo-real } \\leftarrow look-up(A) \\)  (Regla 4)",
+		       "\\( \\mbox{&lt;expr&gt;.tipo-esperado } \\leftarrow \\mbox{&lt;var&gt;.tipo-real } \\) (Regla 1)",
 		      item(
 			  [
-			      "\\(  < var >[2].tipo-real \\leftarrow look-up(A) \\) (Regla 4)",
-			      "\\(  < var >[3].tipo-real \\leftarrow look-up(B) \\) (Regla 4)"
+			      "\\(  \\mbox{&lt;var&gt;[2].tipo-real }\\leftarrow look-up(A) \\) (Regla 4)",
+			      "\\(  \\mbox{&lt;var&gt;[3].tipo-real }\\leftarrow look-up(B) \\) (Regla 4)"
 			  ]),
-		      "\\( < expr >.tipo-real \\leftarrow \\) int o real  (Regla 2)",
-		      "\\( < expr >.tipo-esperado = < expr >.tipo-real  \\) es VERDADERO o FALSO (Regla 2)"
+		      "\\( \\mbox{&lt;expr&gt;.tipo-real }\\leftarrow \\) int o real  (Regla 2)",
+		      "\\( \\mbox{&lt;expr&gt;.tipo-esperado } = \\mbox{&lt;expr&gt;.tipo-real } \\) es VERDADERO o FALSO (Regla 2)"
 		  ]),
 	      fig(img("attribgram3.png","auto","auto"))
 	  )).
@@ -235,7 +235,280 @@ semanticaestatica -->
 	  
 
 sematincadinamica -->
-    section("Semántica Dinámica").
+    section("Semántica Dinámica"),
+    slide("90%","Métodos Desarrollados",
+	  item(
+	      [
+		  (
+		      el(b,col(green, "Semántica Operacional")),
+		      item(appear,
+			   [
+			       "Operaciones en una máquina abstracta"
+			   ])
+		  ),
+		  (
+		      el(b,col(green, "Semántica Denotacional")),
+		      item(appear,
+			   [
+			       "Usa funciones para especificar la semántica, los programas se convierten en funciones para poder aplicar la teoría de funciones recursivas"
+			   ])
+		  ),
+		  (
+		      el(b,col(green, "Semántica Axiomática")),
+		      item(appear,
+			   [
+			       "Aplica la lógica formal: afirmaciones (aserciones) para describir suposiciones y resultados deseados",
+			       "Los axiomas o reglas de inferencia son usados en cada tipo de sentencias."
+			   ])
+		  )
+	      ])
+	 ),
+    slide(col(green,"Semántica Operacional"),
+	  (
+	      slide("90%","",
+		    (
+			el(p,"Describe el significado de un programa ejecutando sus sentencias sobre una máquina, simulada o real. Los cambios en el estado de la máquina (registros, memoria, etc) define el significado de la sentencia."),
+			el(p,"Para el uso de una semántica operacional en un lenguaje de alto nivel se necesita una máquina virtual"),
+			item(
+			    [
+				"Un intérprete de hardware puro podría ser muy costoso.",
+				"Un intérprete de software puro también tiene problemas (dependiente de la máquina )"
+			    ])
+		    )),
+	      slide("",
+		    (
+			el(p,"Una mejor alternativa: Una simulación completa de la computadora"),
+			item(
+			    [
+				"Construir un traductor del codigo fuente a un codigo maquina de una computadora idealizada",
+				"Construir un simulador para la computadora idealizada"
+			    ])
+		    )),
+	      slide(col(green,"Semántica Operacional"),
+		    item(
+			[
+			    (
+				"Simulador de Prolog en Prolog",
+				code(
+				    (
+					"mi(true).\n",
+					"mi((A,B)) :-\n",
+					"        mi(A),\n",
+					"        mi(B).\n",
+					"mi(Goal) :-\n",
+					"        Goal \\= true,\n",
+					"        Goal \\= (_,_),\n",
+					"        clause(Goal, Body),\n",
+					"        mi(Body).\n"
+				    ))
+			    ),
+			    (
+				"Evaluación:",
+				item(
+				    [
+					"Bueno usado informalmente.",
+					"Extremadamente complejo usado formalmente."
+				    ])
+			    )
+			])
+		   )
+	  )),
+    slide("",
+	  (
+	      slide("85%",col(green,"Semántica Denotacional"),
+		    item('highlight-current-green',
+			 [
+			     "Basado en la teoría de funciones recursivas",
+			     "El método de descripción semántica mas abstracto",
+			     "Originalmente desarrollado por Scott y Strachey (1970)",
+			     (	 "El proceso de construir una especificación denotacional para un lenguaje es definir un objeto matemático por cada entidad del Lenguaje",
+				 item(["Define una función que relaciona instancias de las entidades del lenguaje con instancias de los objetos matemáticos correspondientes"])),
+			     "El significado de las construcciones del lenguaje son definidos solo por los valores de las variables del programa"
+			 ])
+		   ),
+	      slide("85%",col(green,"Semántica Denotacional vs Semántica Operacional"),
+		    item('highlight-current-green',
+			 [
+			     "En la semántica operacional los cambios de estado son definidos por algoritmos codificados",
+			     "En la semántica denotacional los cambios de estado son definidos por funciones matemáticas rigurosas.",
+			     "El estado de un programa son los valores de todas las variables actuales  \\(s = { < i_1,v_1 >,< i_2,v_2 >, ... ,< i_n,v_n > }\\)", 
+			     	 "Sea \\(VARMAP\\), una función que, cuando recibe un nombre de variable y un estado retorna el valor actual de esa variable \\(VARMAP(i_j, s) = v_j\\)" 
+			 ])
+		   ),
+	      slide(col(red,"Números Decimales"),
+		    item(
+			[
+			    "\\( \\mbox{&lt;dec-num&gt;} \\to 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | \\)",
+			    "\\( M_{dec} ('0') = 0,  M_{dec} ('1') = 1, ... , M_{dec} ('9') = 9 \\)",
+			    "\\( M_{dec} ( \\mbox{&lt;dec-num&gt;} '0') = 10 * M_{dec} ( \\mbox{&lt;dec-num&gt;} ) \\)",
+			    "\\( M_{dec} ( \\mbox{&lt;dec-num&gt;} '1') = 10 * M_{dec} ( \\mbox{&lt;dec-num&gt;} ) + 1 \\)",
+			    "\\(\\ldots\\)",
+			    "\\( M_{dec} ( \\mbox{&lt;dec-num&gt;} '9') = 10 * M_{dec} ( \\mbox{&lt;dec-num&gt;} ) + 9 \\)"
+			])
+		   ),
+	      slide(col(blue,"Expresiones"),
+		    (
+			el(p,"relaciona expresiones a \\(Z \\cup \\{ error \\} \\)"),
+			el(p,"suponiendo que las expresiones son números decimales, variables, o expresiones binarias teniendo un operador aritmético y dos operandos, cada uno de los cuales puede ser una expresión.")
+		    )),
+	      slide("60%","",
+		    enum(
+			[
+			    "\\( M_e ( \\mbox{&lt;expr&gt;}, s) = case \\mbox{&lt;expr&gt;} of \\)",
+			    "\\(\\qquad \\mbox{&lt;dec-num&gt;} \\to M_{dec} ( \\mbox{&lt;dec-num&gt;} , s)\\)",
+			    "\\(\\qquad \\mbox{&lt;var&gt;} \\to \\mbox{ if } VARMAP(\\mbox{&lt;var&gt;}, s)\\)",
+			    "\\(\\qquad \\mbox{&lt;binary-expr&gt;} \\to \\)",
+			    "\\(\\qquad \\qquad \\mbox{ if } (M_e(\\mbox{&lt;binary-expr&gt;} . \\mbox{&lt;left-expr&gt;} , s) = undef\\)",
+			    "\\(\\qquad \\qquad \\qquad \\mbox{ OR } M_e(\\mbox{&lt;binary-expr&gt;} . \\mbox{&lt;right-expr&gt;} , s) = undef)\\)",
+			    "\\(\\qquad \\qquad \\mbox{ then error}\\)",
+			    "\\(\\qquad \\qquad \\mbox{ else}\\)",
+			    "\\(\\qquad \\qquad \\qquad \\mbox{ if } (\\mbox{&lt;binary-expr&gt;}.\\mbox{&lt;operator&gt;} = ‘+’ then \\)" ,
+			    "\\(\\qquad \\qquad \\qquad  M_e(\\mbox{&lt;binary-expr&gt;}.\\mbox{&lt;left-expr&gt;}, s) + M_e(\\mbox{&lt;binary-expr&gt;}.\\mbox{&lt;right-expr&gt;}, s)\\)",
+			    "\\(\\qquad \\qquad \\qquad \\mbox{ else }\\)",
+			    "\\(\\qquad \\qquad \\qquad M_e(\\mbox{&lt;binary-expr&gt;}.\\mbox{&lt;left-expr&gt;}, s) * M_e(\\mbox{&lt;binary-expr&gt;}.\\mbox{&lt;right-expr&gt;}, s)\\)",
+			    "\\( \\ldots \\)"
+			])
+		   ),
+	      slide("85%",col(brown,"asignación"),
+		    enum(
+			[
+			    "\\( M_a ( X := E, s) = \\)",
+			    "\\(\\qquad \\mbox{if } M_e(E, s) = error \\)",
+			    "\\(\\qquad \\qquad \\mbox{ then error}\\)",
+			    "\\(\\qquad \\qquad \\mbox{ else } s' = { < i_{1}',v_{1}' >, < i_{2}',v_{2}' >, \\ldots , < i_{n}',v_{n}' >},\\)",
+			    "\\(\\qquad \\qquad \\qquad \\mbox{ where for } j = 1, 2, \\ldots n,\\)",
+			    "\\(\\qquad \\qquad \\qquad \\qquad v_{j}' = varmap(i_j, s) \\mbox{ if } i_j <> x \\)",
+			    "\\(\\qquad \\qquad \\qquad \\qquad \\qquad  = M_e(E, s) \\mbox{ if } i_j = x\\)"
+			])
+		   ),
+	      slide("85%",col(yellow,"Ciclo 'while'"),
+		    enum(
+			[
+			    "\\( M_l(\\mbox{while B do L}, s) =\\)",
+			    "\\(\\qquad \\mbox{ if } M_b(B, s) = undef\\)",
+			    "\\(\\qquad \\qquad \\mbox{ then error}\\)",
+			    "\\(\\qquad \\qquad \\mbox{ else if } M_b(B, s) = false\\)",
+			    "\\(\\qquad \\qquad \\qquad \\mbox{ then } s\\)",
+			    "\\(\\qquad \\qquad \\qquad \\mbox{ else if } M_{sl}(L, s) = error\\)",
+			    "\\(\\qquad \\qquad \\qquad \\qquad \\mbox{ then error}\\)",
+			    "\\(\\qquad \\qquad \\qquad \\qquad \\mbox{ else } M_l(\\mbox{while B do L}, M_{sl}(L, s))\\)"
+			])
+		   ),
+	      slide("Ciclo",
+		    item('highlight-current-red',
+			 [
+			     "El significado del ciclo es el valor de las variables del programa después de que las sentencias del ciclo han sido ejecutadas el número prescrito de veces, asumiendo que no ha habido errores",
+			     "En esencia el ciclo ha sido convertido de iterativo a recursivo, donde el control recursivo es definido por otra función recursiva de estados",
+			     "La recursión comparada con la iteración es mas facil de describir con rigor matemático"
+			 ])
+		   ),
+	      slide(col(green,"Evaluación"),
+		    item('highlight-current-green',
+			 [
+			     "Puede ser usado para probar la corrección de programas",
+			     "Provee un modo riguroso de pensar los programas",
+			     "Puede ser una ayuda al diseño de lenguajes",
+			     "Ha sido usado en sistemas de generación de compiladores",
+			     "A causa de su complejidad es de poco uso para los usuarios del lenguaje"
+			 ])
+		   )
+	  )),
+        slide("",
+	      (
+		  slide("Semántica Axiomática",
+			item('fade-in-then-semi-out',
+			    [
+				"Basado en Lógica Formal (cálculo de predicados)",
+				"Propósito original: Verificación formal de programas",
+				"Axiomas o reglas de inferencia son definidas para cada tipo de sentencia del lenguaje (para permitir transformaciones de expresiones a otras expresiones)",
+				(   "Las expresiones son llamadas ", el(em,"aserciones"), " (afirmaciones) " )
+			    ])
+		       ),
+		  slide(col(blue,"Semántica Axiomática"),
+			item('highlight-current-blue',
+			     [
+				 (   "Una aserción antes de una sentencia (una ", el(em,"precondición"), "establece las relaciones y restricciones entre variables que son verdaderas en ese punto de la ejecución" ),
+				 (   "Una aserción que sigue a una sentencia es una ", el(em,"postcondición") ),
+				 (   "Una ", el(em,"precondición mas débil"), " es la menos restrictiva precondición que garantiza la postcondición" )
+			    ])
+		       ),
+		  slide(col(red,"Semántica Axiomática"),
+			item(appear,
+			     [
+				 "La Forma es \\({P}\\) sentencia \\({Q}\\)",
+				 (   "Un ejemplo" ,
+				     item(appear,
+					  [
+					      "\\(a = b + 1 \\: \\{a > 1\\} \\)",
+					      "una posible precondición: \\( \\{b > 10\\} \\)",
+					      (	  el(em,"precondición mas débil"), "\\( \\{b > 0\\} \\)" ) 
+					  ])
+				 )
+			     ])
+		       ),
+		  slide(col(brown,"Proceso de prueba de programa"),
+			(
+			    el(p,"La postcondición para el programa entero es el resultado deseado"),
+			    item(appear,
+				 [
+				     "Se trabaja hacia atrás a través del programa hasta la primer sentencia. Si la precondición sobre la primer sentencias está inferida por la especificación de entrada del programa, entonces el programa es correcto."
+				 ])
+			)
+		       ),
+		  slide("Axiomas",
+			item(appear,
+			    [
+				(   
+				    "Un axioma para la asignación",
+				    item(
+					[
+					    "( x = E ): \\( \\{Q_{x \\to E}\\} \\ x = E \\ \\{Q\\} \\)"
+					])
+				),
+				(   
+				    "La regla de la Consecuencia",
+				    item(
+					[
+					    " \\[ \\frac{ \\{P\\} \\ S \\ \\{Q\\}, P' \\Rightarrow P, Q \\Rightarrow Q'}{\\{P'\\} \\ S \\ \\{Q'\\}} \\] "
+					])
+				)
+			    ])
+		       ),
+		  slide("Axiomas",
+			item(appear,
+			     [
+				 "\\( x = 2 * y - 3 \\{x > 25 \\}  \\)",
+				 "\\( 2 * y - 3 > 25  \\)",
+				 "\\( y > 14  \\)",
+				 "\\( x = x + y - 3 \\{x > 10 \\}  \\)",
+				 "\\( x + y - 3 > 10  \\)",
+				 "\\( y > 13 - x  \\)"
+			     ])
+		       ),
+		  slide("90%","Axiomas",
+			" \\[ \\frac{ \\{x > 3\\} \\ x = x - 3 \\ \\{x > 0\\}, (x > 5) \\Rightarrow (x > 3), (x > 0) \\Rightarrow (x > 0)}{\\{x > 5\\} \\ x = x - 3 \\ \\{x > 0\\}} \\]"
+		       ),
+		  slide("Axiomas",
+			(
+			    item(
+				[
+				    (
+					"Una regla de inferencia para secuencias",
+					item(
+					    [
+						  "\\( \\{P1\\} S1 \\{P2\\} \\)",
+						  "\\( \\{P2\\} S2 \\{P3\\} \\)"
+					    ])
+				    )
+				]),
+			    "\\[ \\frac{ \\{P1\\} \\ S1 \\ \\{P2\\}, \\{P2\\} \\ S2 \\ \\{P3\\}}{\\{P1\\} \\ S1;S2 \\ \\{P3\\}} \\]"
+			
+			))
+	      )).
+
+
+
+
 
 
 
